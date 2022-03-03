f12a9acb3919769bb923ed25fdaac84bd6864463
Havur <haavaaln@stud.ntnu.no>
2022-03-03 15:29:54 +0100

    ass

--- make_person.hpp
+++ make_person.hpp
@@ -2,7 +2,20 @@
 // Created by Håvard Surface on 03.03.2022.
 //
 
+#include <string>
+
 #ifndef ANCESTOR_TREE_MAKE_PERSON_HPP
 #define ANCESTOR_TREE_MAKE_PERSON_HPP
+
+namespace ancestor_tree{
+    class make_person{
+    public:
+
+
+    private:
+        std::string<age_>;
+
+    };
+}
 
 #endif //ANCESTOR_TREE_MAKE_PERSON_HPP
