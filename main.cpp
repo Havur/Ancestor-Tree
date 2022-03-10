f47abd0ea349cab6d4bf8e5a28d01df3a2e99e79
Robertalmo <roberam@stud.ntnu.no>
2022-03-10 14:03:26 +0100

    Did some changes in hpp, and moved functions to cpp.

--- main.cpp
+++ main.cpp
@@ -10,18 +10,44 @@
  *
  * 2.
  */
-
-#include "make_person.hpp"
-
 #include <iostream>
 #include <algorithm>
 #include <map>
 #include <unordered_map>
 #include <string>
-
+#include "make_person.hpp"
 namespace ancestor_tree{
 
+    void make_person::insert_name() {
+        std::cout << "Plot your name: ", std::cin >> name_, std::cout << "Name: " << name_ << std::endl; //Gets name from user and locates it in name_
+
+    }
+    void make_person::insert_birthYear() {
+        std::cout << "Plot your Year of birth: ", std::cin >> birthYear_, std::cout << "Birth Year: " << birthYear_ << std::endl; //Gets Birth year from user and locates it in birthYear
+    }
+
+    void make_person::insert_deathYear() {
+        std::cout << "Plot your Year of death (if still alive type -1): " , std::cin >> deathYear_;
+    }
+
+
+    void make_person::insert_age() {
+        if (deathYear_ > 0) {
+            age_ = deathYear_ - birthYear_;
+            std::cout << "Age: " << age_ << std::endl;
+        }
+        else{
+            age_= currentYear_ - birthYear_;
+            std::cout<< "Age: " << age_ <<std::endl;
+        }
+
+    }
+
+    make_person::make_person() {
+
+    }
 }
+
 int main(){
    ancestor_tree::make_person mainPerson;
    mainPerson.insert_name();
--- make_person.hpp
+++ make_person.hpp
@@ -7,47 +7,28 @@
 #ifndef ANCESTOR_TREE_MAKE_PERSON_HPP
 #define ANCESTOR_TREE_MAKE_PERSON_HPP
 
-namespace ancestor_tree{
-    class make_person{
+namespace ancestor_tree {
+    class make_person {
 
     private:
         std::string name_{""};
         int birthYear_{0};
-        int age_{0} ;
+        int age_{0};
         int deathYear_{0};
         std::string parent_{""};
         int currentYear_{2022};
 
     public:
+        make_person();
 
-    void insert_name() {
+        void insert_name();
 
-        std::cout << "Plot your name: ", std::cin >> name_, std::cout << "Name: " << name_ << std::endl; //Gets name from user and locates it in name_
+        void insert_birthYear();
 
-    }
-    void insert_birthYear() {
-        std::cout << "Plot your Year of birth: ", std::cin >> birthYear_, std::cout << "Birth Year: " << birthYear_ << std::endl;
-    }
+        void insert_deathYear();
 
-    void insert_deathYear() {
-
-        std::cout << "Plot your Year of death (if still alive type -1): " , std::cin >> deathYear_;
-    }
-
-
-        void insert_age() {
-            if (deathYear_ > 0) {
-                age_ = deathYear_ - birthYear_;
-                std::cout << "Age: " << age_ << std::endl;
-            }
-            else{
-                age_= currentYear_ - birthYear_;
-                std::cout<< "Age: " << age_ <<std::endl;
-            }
-
-        }
+        void insert_age();
     };
 }
 
 #endif //ANCESTOR_TREE_MAKE_PERSON_HPP
-
