6db7fd6b5f915af7a6e185a3cf9b4e757256ba38
Robertalmo <roberam@stud.ntnu.no>
2022-05-03 12:37:52 +0200

    Removed shared pointer from root node cuz not needed. Fixed createPerson function so that it doesn´t glitch if input is something else than string or int.

--- application/main.cpp
+++ application/main.cpp
@@ -1,126 +1,79 @@
 #include <iostream>
 #include <optional>
-
+#include <string>
 #include "person.hpp"
 #include "personBuilder.hpp"
 #include "tree.hpp"
 
-//bool hasRootNode = false;
-//
-//person createPerson(){
-//    std::string name_;
-//    std::cout << "Type name: " ;
-//    if(hasRootNode) {
-//        std::cin.ignore();
-//    } else {
-//        hasRootNode = true;
-//    }//stupid fix, this fixes so the the first character in root person name is not skipped...
-//
-//    std::getline(std::cin, name_);
-//    std::cout << "Name: " <<  name_ << std::endl;
-//
-//    int birthYear_;
-//    std::cout << "Plot the year of birth: ";
-//    std::cin >> birthYear_;
-//
-//    std::optional <int> deathYear;
-//    std::cout << "Is person still alive? yes/no: ";
-//    std::string aliveInput;
-//    std::cin >> aliveInput;
-//
-//    if (aliveInput == "yes" || aliveInput == "y"){
-//        //bool alive = true;
-//    }
-//    else if (aliveInput == "no" || aliveInput == "n") {
-//        std::cout << "Plot the year of death: ";
-//        int deathYearTemp;
-//        std::cin >> deathYearTemp;
-//        deathYear = deathYearTemp;
-//    }
-//    else {
-//        std::cout<<"Wrong input"<<std::endl;
-//    }
-//
-//
-//    std::string gender_;
-//    std::cout << "Insert gender m/f/?: ";
-//    std::cin >> gender_;
-//    if (gender_ == "m") {
-//        gender_ = "Male";
-//    } else if (gender_ == "f") {
-//        gender_ = "Female";
-//    } else {
-//        std::cout << "Insert own gender: ";
-//        std::cin >> gender_;
-//    }
-//    return person{name_,gender_,birthYear_,deathYear};
-//}
-int main() {
+    int main() {
 
-    std::cout << "Welcome to the family tree. \n\n"
-                 "Lets start off by creating the main person in the tree >:D \n";
+        std::cout << "Welcome to the family tree. \n\n"
+                     "Lets start off by creating the main person in the tree >:D \n";
 
 
-   auto root = std::make_shared<nodeTree>(ancestorTree::personBuilder::createPerson());
-   //auto mother = std::make_shared<nodeTree>();
-   //auto father = std::make_shared<nodeTree>();
-   //root->addFather(father);
-  //root->addMother(mother);
+        auto root = nodeTree(ancestorTree::createPerson(false));
 
-    bool quit = false;
-    while (!quit) {
-        std::cout << "Main menu:"
-                  << "\n";
 
-        std::cout << "1: Print tree"
-                  << "\n";
-        std::cout << "2: Print individual"
-                  << "\n";
-        std::cout << "3: Add ancestor"
-                  << "\n";
-        std::cout << "0: Exit"
-                  << "\n";
+        bool quit = false;
+        while (!quit) {
+            std::cout << "Main menu:"
+                      << "\n";
 
-        int answer;
-        std::cin >> answer;// Possible issue here. What if user input is not a number?
-        //ta inn en string isteden for int og sjekk om stringen e ett tall
-        switch (answer) {
-            case 1:
+            std::cout << "1: Print tree"
+                      << "\n";
+            std::cout << "2: Print individual"
+                      << "\n";
+            std::cout << "3: Add ancestor"
+                      << "\n";
+            std::cout << "0: Exit"
+                      << "\n";
 
-                break;
-
-            case 2: {
-                std::shared_ptr<nodeTree> left;
-                break;
+            std::string answer;
+            std::cin>> answer;
+            int input = -1 ;       //Setting input as -1 then it will return default case input if input is string.
+            try {
+                 input = std::stoi(answer);
+            } catch (std::exception& ex){
             }
-            case 3: {
 
-                auto person = ancestorTree::personBuilder::createPerson();
-                if (person.getGender() == "m"){
-                   root->addFather(std::make_shared<nodeTree>(person));}
-                else if (person.getGender() == "f"){
-                    root->addMother(std::make_shared<nodeTree>(person));}
-                std::cout << "Name of closest relative" << std::endl;
-                std::string relative;
-                std::cin.ignore();
-                std::getline(std::cin, relative);
-                std::cout << "Name: " << relative << std::endl;
+            switch (input) {
+                case 1:
+
+                    break;
+
+                case 2: {
+                    std::shared_ptr<nodeTree> left;
+                    break;
+                }
+                case 3: {
+
+                    auto person = ancestorTree::createPerson(true);
+                    if (person.getGender() == "m") {
+                        root.addFather(std::make_shared<nodeTree>(person));
+                    } else if (person.getGender() == "f") {
+                        root.addMother(std::make_shared<nodeTree>(person));
+                    }
+                    std::cout << "Name of closest relative" << std::endl;
+                    std::string relative;
+                    std::cin.ignore();
+                    std::getline(std::cin, relative);
+                    std::cout << "Name: " << relative << std::endl;
 
 
-                break;
+                    break;
+                }
+                case 0:
+                    quit = true;
+                    break;
+                default:
+                    std::cout << "Unknown answer: '" << answer << "'"
+                              << "\n"
+                              << "Pleas try again.."
+                              << "\n\n";
+                    break;
             }
-            case 0:
-                quit = true;
-                break;
-            default:
-                std::cout << "Unknown answer: '" << answer << "'"
-                          << "\n"
-                          << "Pleas try again.."
-                          << "\n\n";
-                break;
         }
+
+
+        return 0;
     }
-
-
-    return 0;
-}
--- application/personBuilder.hpp
+++ application/personBuilder.hpp
@@ -1,5 +1,4 @@
 #include "person.hpp"
-
 #include <iostream>
 #include <map>
 #include <string>
@@ -8,101 +7,64 @@
 #define ANCESTOR_TREE_MAKE_PERSON_HPP
 
 namespace ancestorTree {
-    class personBuilder : public person {
-    private:
+    static person createPerson(bool hasRootNode) {
         std::string name_;
-        std::string gender_;
-        int birthYear_;
-        std::optional<int> deathYear_;
-
-    public:
-        static person createPerson() {
-
-            std::string name_;
-            std::cout << "Type name: ";
+        std::cout << "Type name: ";
+        if (hasRootNode) {
             std::cin.ignore();
-            std::getline(std::cin, name_);
-            std::cout << "Name: " << name_ << std::endl;
+        }                       //this fixes so the the first character in root person name is not skipped...
+        std::getline(std::cin, name_);
+        std::cout << "Name: " << name_ << std::endl;
 
-            int birthYear_;
+        int birthYear_ = -1;
+        while (birthYear_ == -1) {
+            std::string input;
             std::cout << "Plot the year of birth: ";
-            std::cin >> birthYear_;
-
-            std::optional<int> deathYear;
-            std::cout << "Is person still alive? yes/no: ";
-            std::string aliveInput;
-            std::cin >> aliveInput;
-
-            if (aliveInput == "yes" || aliveInput == "y") {
-                //bool alive = true;
-            } else if (aliveInput == "no" || aliveInput == "n") {
-                std::cout << "Plot the year of death: ";
-                int deathYearTemp;
-                std::cin >> deathYearTemp;
-                deathYear = deathYearTemp;
-            } else {
-                std::cout << "Wrong input" << std::endl;
+            std::cin >> input;
+            try {
+                birthYear_ = std::stoi(input);
+            } catch (std::exception &ex) {
+                std::cout<< "Unknown input, add a valid birthyear." << std::endl;
             }
-
-
-            std::string gender_;
-            std::cout << "Insert gender m/f/?: ";
-            std::cin >> gender_;
-            if (gender_ == "m") {
-                gender_ = "Male";
-            } else if (gender_ == "f") {
-                gender_ = "Female";
-            } else {
-                std::cout << "Insert own gender: ";
-                std::cin >> gender_;
-            }
-            return person{name_, gender_, birthYear_, deathYear};
         }
-        //      person create() {
-        //          return {birthYear_, name_, gender_};
-        //      }
 
-        //          void insertName() {
-        //              std::cout << "Plot your name: ";
-        //              std::cin.ignore();
-        //              std::getline(std::cin, name_);
-        //              std::cout << "Name: " << name_ << std::endl;//Gets name from user and locates it in name_
+        std::optional<int> deathYear;
+        std::cout << "Is person still alive? yes/no: ";
+        std::string aliveInput;
+        std::cin >> aliveInput;
 
-        //          }
+        if (aliveInput == "yes" || aliveInput == "y") {
 
-        //          void insertBirthYear() {
-        //              std::cout << "Plot your Year of birth: ";
-        //              std::cin >> birthYear_;
-        //          }
-        //          void insertDeathYear() {
-        //              std::cout << "Plot your Year of death (if still alive type -1): ";
-        //              std::cin >> deathYear_;//NB! Needs to be rebuilt
-        //          }
-
-        //      //  void insertAge() {
-        //      //      if (deathYear_ > 0) {
-        //      //          age_ = deathYear_ - birthYear_;
-        //      //          std::cout << "Age: " << age_ << std::endl;
-
-        //      //      } else {
-        //      //          age_ = currentYear_ - birthYear_;
-        //      //          std::cout << "Age: " << age_ << std::endl;
-        //      //      }
-        //      //  }
-        //          void insertGender() {
-        //              std::cout << "Insert your gender m/f/?: ";
-        //              std::cin >> gender_;
-        //              if (gender_ == "m") {
-        //                  gender_ = "Male";
-        //              } else if (gender_ == "f") {
-        //                  gender_ = "Female";
-        //              } else {
-        //                  std::cout << "Insert your own gender: ";
-        //                  std::cin >> gender_;
-        //              }
+        } else if (aliveInput == "no" || aliveInput == "n") {
+            int deathYearTemp = -1;
+            while (deathYearTemp == -1){
+                std::string input;
+                std::cout << "Plot the year of death: ";
+                std::cin >> input;
+                try {
+                    deathYearTemp = std::stoi(input);
+                } catch (std::exception &ex) {
+                    std::cout<< "Unknown input, add a valid birthyear." << std::endl;
+                }
+            }
+        } else {
+        }
 
 
-        //  };
-    };}
+        std::string gender_;
+        std::cout << "Insert gender m/f/?: ";
+        std::cin >> gender_;
+        if (gender_ == "m") {
+            gender_ = "Male";
+        } else if (gender_ == "f") {
+            gender_ = "Female";
+        } else {
+            std::cout << "Insert own gender: ";
+            std::cin >> gender_;
+        }
+        return person{name_, gender_, birthYear_, deathYear};
+    }
+
+}
 
 #endif//ANCESTOR_TREE_MAKE_PERSON_HPP
