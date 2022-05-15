2a7da4a474bd7370a1c63dcdd24799eefb5fd031
Robertalmo <roberam@stud.ntnu.no>
2022-05-15 14:35:49 +0200

    Added some unittests to the project. To se how print individual and print tree works.

--- tests/TestPerson.cpp
+++ tests/TestPerson.cpp
@@ -1,4 +1,5 @@
 #define CATCH_CONFIG_MAIN
+
 #include "catch.hpp"
 
 #include "Person.hpp"
@@ -8,26 +9,46 @@
 #include <sstream>
 
 TEST_CASE("Print induvidual") {
-    Person person{"Ola", Gender::male, 2000, 0};
-    std::cout<< person << std::endl;
+    Person person{"Ola", Gender::male, 2000, 2010};
+    std::cout << person << std::endl;
+    std::cout << "──────────────────────────────────────────" << "\n";
 }
+
 TEST_CASE("Print tree") {
+
+    std::cout << "TEST FOR PRINTING TREE:" << "\n";
+    std::cout << "\n";
     nodeTree ola{Person{"Ola", Gender::male, 2000, 0}};
 
     auto connie = std::make_shared<nodeTree>(Person{"Connie", Gender::male, 2000, 0});
-    auto berit = std::make_shared<nodeTree>(Person{"Berit", Gender::female, 2000, 0});    ola.addLeft(connie);
-    ola.addRight(connie);
-    ola.addLeft(berit);
+    auto berit = std::make_shared<nodeTree>(Person{"Berit", Gender::female, 2000, 0});
+    ola.addLeft(connie);
+    ola.addRight(berit);
 
- nodeTree marit{Person{"Marit", Gender::female, 2000, 0}};
 
-   auto synne = std::make_shared<nodeTree>(Person{"Synne", Gender::female, 2000, 0});
-   auto bob = std::make_shared<nodeTree>(Person{"Bob", Gender::male, 2000, 0}); //
- //
- //  nodeTree åge{Person{"Åge", Gender::male, 2000, 0}};
- //
- //  nodeTree guro{Person{"Guro", Gender::female, 2000, 0}};
- //  nodeTree peter{Person{"Peter", Gender::male, 2000, 0}}
+    auto synne = std::make_shared<nodeTree>(Person{"Synne", Gender::female, 2000, 0});
+    auto bob = std::make_shared<nodeTree>(Person{"Bob", Gender::male, 2000, 0}); //
+    connie->addRight(synne);
+    connie->addLeft(bob);
+
+
+    auto marit = std::make_shared<nodeTree>(Person{"Marit", Gender::female, 2000, 0});
+    auto peder = std::make_shared<nodeTree>(Person{"Peder", Gender::male, 2000, 0});
+    berit->addRight(marit);
+    berit->addLeft(peder);
+
+
+    auto beate = std::make_shared<nodeTree>(Person{"Beate", Gender::female, 2000, 0});
+    auto per = std::make_shared<nodeTree>(Person{"Per", Gender::male, 2000, 0});
+    bob->addRight(beate);
+    bob->addLeft(per);
+
+
+    auto kirsti = std::make_shared<nodeTree>(Person{"Kirsti", Gender::female, 2000, 0});
+    auto arnold = std::make_shared<nodeTree>(Person{"Arnold", Gender::male, 2000, 0});
+    synne->addRight(kirsti);
+    synne->addLeft(per);
+    ola.printTree();
 
 
 }
